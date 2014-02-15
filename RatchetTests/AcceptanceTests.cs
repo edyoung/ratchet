6e979b78b1dc334c274bc81ed1fbc5708c715038
Edwin Young <edyoung@microsoft.com>
2014-02-15 10:53:54 -0800

    first failing acceptance test

---
+++ RatchetTests/AcceptanceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RatchetTests
+{
+    /// <summary>
+    /// Higher level acceptance tests covering overall functionality
+    /// </summary>
+    [TestClass]
+    public class AcceptanceTests
+    {
+        private const string KnownWarning = "This is a known warning";
+        private const string UnknownWarning = "This is an unknown warning";
+
+        [TestMethod]
+        public void ReadWarningsFromInputStream_FilterOutKnownWarningsAndErrorOnUnknown()
+        {
+            ProcessStartInfo pInfo = new ProcessStartInfo("ratchet.exe");
+            pInfo.WorkingDirectory = System.Environment.CurrentDirectory;
+            pInfo.UseShellExecute = false;
+            pInfo.RedirectStandardInput = true;
+            pInfo.RedirectStandardError = true;
+            
+            string inputWarnings = String.Join("\n", KnownWarning, UnknownWarning);
+
+            Process p = Process.Start(pInfo);
+            p.StandardInput.Write(inputWarnings);
+            p.StandardInput.Close();
+            string errors = p.StandardError.ReadToEnd();
+            
+            Assert.AreEqual(UnknownWarning, errors);
+        }
+    }
+}
