84eac77316cda0ea93823491135cfbe5ef741ab1
Edwin Young <edyoung@microsoft.com>
2014-02-16 06:52:07 -0800

    added some warnings

--- ExampleProjectWithWarnings/Class1.cs
+++ ExampleProjectWithWarnings/Class1.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ExampleProjectWithWarnings
 {
-    public class Class1
+    // from http://msdn.microsoft.com/en-us/library/4bw5ewxy.aspx
+    // produces CS0420
+    public class TestClass
     {
+        private volatile int i;
+
+        public void TestVolatile(ref int ii)
+        {
+        }
+
+        public static void Main()
+        {
+            TestClass x = new TestClass();
+            x.TestVolatile(ref x.i);   // CS0420 
+        }
+
     }
 }
